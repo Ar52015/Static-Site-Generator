4028a366fb3fd01e7d20192b9a7513e6ffa66505
Ar52015 <aryan.arora2015@gmail.com>
2025-03-26 13:23:24 +0000

    added leadnode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     
@@ -30,6 +30,28 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIn(' class="button"', html)
         # Count the number of spaces to ensure properties are properly separated
         self.assertEqual(html.count(' '), 3)  # One space before each of the 3 properties
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+    
+    def test_leaf_to_html_a_with_href(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+    
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Just some text")
+        self.assertEqual(node.to_html(), "Just some text")
+    
+    def test_leaf_to_html_with_multiple_attributes(self):
+        node = LeafNode("input", "", {"type": "text", "placeholder": "Enter your name"})
+        self.assertEqual(node.to_html(), '<input type="text" placeholder="Enter your name"></input>')
+    
+    def test_leaf_to_html_no_value_raises_error(self):
+        node = LeafNode("div", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
 
 if __name__ == "__main__":
     unittest.main()
