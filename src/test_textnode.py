426717bfa0ec716da8d5001ff0243c7241441405
Ar52015 <aryan.arora2015@gmail.com>
2025-04-05 13:49:49 +0000

    Partitioned Functions

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimiter
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -109,43 +109,6 @@ class TestTextNode(unittest.TestCase):
         except Exception:
             # Test passes if an exception is raised
             pass
-
-    def test_split_nodes_delimiter_no_delimiters(self):
-        # Test with no delimiters present
-        node = TextNode("This is just plain text", TextType.NORMAL_TEXT)
-        nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
-        assert len(nodes) == 1
-        assert nodes[0].text == "This is just plain text"
-        assert nodes[0].textType == TextType.NORMAL_TEXT
-
-    def test_split_nodes_delimiter_one_delimiter_pair(self):
-        # Test with one pair of delimiters
-        node = TextNode("This is **bold** text", TextType.NORMAL_TEXT)
-        nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
-        assert len(nodes) == 3
-        assert nodes[0].text == "This is "
-        assert nodes[0].textType == TextType.NORMAL_TEXT
-        assert nodes[1].text == "bold"
-        assert nodes[1].textType == TextType.BOLD_TEXT
-        assert nodes[2].text == " text"
-        assert nodes[2].textType == TextType.NORMAL_TEXT
-
-    def test_split_nodes_delimiter_multiple_delimiter_pairs(self):
-        # Test with multiple delimiter pairs
-        node = TextNode("This is **bold** and **more bold** text", TextType.NORMAL_TEXT)
-        nodes = split_nodes_delimiter([node], "**", TextType.BOLD_TEXT)
-        assert len(nodes) == 5
-        assert nodes[0].text == "This is "
-        assert nodes[0].textType == TextType.NORMAL_TEXT
-        assert nodes[1].text == "bold"
-        assert nodes[1].textType == TextType.BOLD_TEXT
-        assert nodes[2].text == " and "
-        assert nodes[2].textType == TextType.NORMAL_TEXT
-        assert nodes[3].text == "more bold"
-        assert nodes[3].textType == TextType.BOLD_TEXT
-        assert nodes[4].text == " text"
-        assert nodes[4].textType == TextType.NORMAL_TEXT
-
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -46,20 +46,3 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
         case _:
             raise Exception("Invalid Text Type")
-        
-def split_nodes_delimiter(old_nodes, delimiter, textType):
-    finalList = []
-    for node in old_nodes:
-        if node.textType is not TextType.NORMAL_TEXT:
-            finalList.append(node)
-        else:
-            parts = node.text.split(delimiter)
-            if len(parts)%2 == 0:
-                raise Exception(f"Incorrect Markdown Syntax: {delimiter} isn't closed")
-
-            for i, text in enumerate(parts):
-                if i%2 == 0:
-                    finalList.append(TextNode(text, TextType.NORMAL_TEXT))
-                else:
-                    finalList.append(TextNode(text, textType))
-    return finalList
