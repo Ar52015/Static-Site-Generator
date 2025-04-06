ca0a08d22dcdd4c9fcf908dc16f70fa5989212da
Ar52015 <aryan.arora2015@gmail.com>
2025-04-06 10:42:15 +0000

    added intermediate file copying

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-import textnode as tn
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    dummy = tn.TextNode("some text", tn.TextType.LINKS, "https://google.com")
-    print(dummy)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 if __name__ == "__main__":
     main()
